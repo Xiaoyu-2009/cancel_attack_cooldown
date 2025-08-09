cb7f8f6fc88c1c82c6abf593548b3c6f5d0cb78f
小宇不是鱼 <3551371733@qq.com>
2025-08-09 22:09:43 +0800

    Add files via upload

--- src/main/java/cancel_attack_cooldown/CancelAttackCooldownMod.java
+++ src/main/java/cancel_attack_cooldown/CancelAttackCooldownMod.java
@@ -1,6 +1,6 @@
 package cancel_attack_cooldown;
 
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 
 @Mod("cancel_attack_cooldown")
 public class CancelAttackCooldownMod {
